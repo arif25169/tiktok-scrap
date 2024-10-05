2a2eb5b1842d3a6cdae9a4fa07ed706d85e28cc9
Arif Hasan <arif25169@gmail.com>
2024-10-05 15:00:49 +0600

    Update TableView.jsx

--- frontend/src/TableView.jsx
+++ frontend/src/TableView.jsx
@@ -4,42 +4,47 @@ import { Link } from "react-router-dom";
 import { Table } from "antd";
 
 function TableView() {
-    let [data, setData] = useState([])
+  let [data, setData] = useState([])
 
-    useEffect(() => {
-        fetch("http://localhost:5000/api")
-            .then(response => response.json())
-            .then(data => setData(data))
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, [])
 
-    const columns = [
-        {
-          title: 'User Name',
-          dataIndex: 'username',
-          key: 'username',
-        },
-        {
-          title: 'Follower Count',
-          dataIndex: 'followerCount',
-          key: 'followerCount',
-        },
-        {
-          title: 'Like Count',
-          dataIndex: 'likeCount',
-          key: 'likeCount',
-        },
-      ];
+  const columns = [
+    {
+      title: 'User Name',
+      dataIndex: 'username',
+      key: 'username',
+    },
+    {
+      title: 'Follower Count',
+      dataIndex: 'followerCount',
+      key: 'followerCount',
+    },
+    {
+      title: 'Like Count',
+      dataIndex: 'likeCount',
+      key: 'likeCount',
+    },
+  ];
 
-    return (
-        <>
-            <h3>
-                <Link to={'/'} unstable_viewTransition>
-                    Chart  View
-                </Link>
-            </h3>
-            <Table dataSource={data} columns={columns} />;
-        </>
-    )
+  return (
+    <>
+      <span>
+        List of Data: <br />
+        - Have at least 100k followers. <br />
+        - Have received over 1 million like
+      </span>
+      <h3>
+        <Link to={'/'} unstable_viewTransition>
+          Chart  View
+        </Link>
+      </h3>
+      <Table dataSource={data} columns={columns} />;
+    </>
+  )
 }
 
 export default TableView
